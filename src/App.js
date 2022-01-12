53075669161cc664e23d381b02328fc782e58e33
Amkam <amaduskamara@gmail.com>
2022-01-12 14:11:01 +0000

    Removed commented import in components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import { Outlet } from 'react-router-dom';
 import Home from './pages/Home';
-// import Calculator from './components/Calculator';
-// import NavBar from './components/NavBar';
 
 // eslint-disable-next-line react/prefer-stateless-function
 export default class App extends Component {
